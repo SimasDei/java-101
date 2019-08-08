b8f1472b80e5de49198fedab07f6553743b4ed4d
Simas <simonas.deimantavicius@mediapark.com>
2019-08-08 08:46:43 +0300

    add number guessing game

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -1,8 +1,30 @@
 package academy.learnprogramming;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = 2;
+        int secondNumber = 5;
+        String prompt = ". Press ENTER when ready";
+        var subtraction = 7;
+        int answer;
+
+        System.out.println("Think of a number between 1 and 10" + prompt);
+        scanner.nextLine();
+        System.out.println("Multiply your number by " + firstNumber + prompt);
+        scanner.nextLine();
+        System.out.println("No multiply the result by " + secondNumber + prompt);
+        scanner.nextLine();
+        System.out.println("Divide the result by the number you originally thought of by " + prompt);
+        scanner.nextLine();
+        System.out.println("Now subtract " + subtraction + prompt);
+        scanner.nextLine();
+
+        scanner.close();
     }
 }
