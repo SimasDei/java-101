524350883256ba5411aacdd1f00189206886da6c
Simas <simonas.deimantavicius@mediapark.com>
2019-08-08 08:14:46 +0300

    variables branch commit

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -1,22 +1,8 @@
 package academy.learnprogramming;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        // Class used to scan/read user input
-        var scanner = new Scanner(System.in);
 
-        System.out.println(Doctor.intro());
-        var userInput = "";
-
-        while (!userInput.equalsIgnoreCase("quit")) {
-            userInput = scanner.nextLine();
-            String response = Doctor.response(userInput);
-            System.out.println(response);
-        }
-
-        scanner.close();
     }
 }
